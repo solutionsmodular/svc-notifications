217cd2036235dbf9c54cb1f2ae483d3688393078
Niqui Eastman <president@solutionsmodular.com>
2022-06-29 22:25:42 -0600

    Starting the process of lining up the NotificationDispatcher

--- src/main/java/com/solmod/notification/engine/service/NotificationDispatcher.java
+++ src/main/java/com/solmod/notification/engine/service/NotificationDispatcher.java
@@ -1,19 +1,14 @@
 package com.solmod.notification.engine.service;
 
 import com.solmod.notification.admin.data.MessageTemplatesRepository;
-import com.solmod.notification.domain.MessageTemplate;
 import com.solmod.notification.domain.SolCommunication;
 import com.solmod.notification.domain.SolMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.MessageChannel;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 import java.util.function.Function;
 
 @Service("NotificationDispatcher")
@@ -32,6 +27,17 @@ public class NotificationDispatcher implements Function<SolMessage, List<SolComm
     public List<SolCommunication> apply(SolMessage solMessage) {
         log.info("Running NotificationDispatcher for {}:{}", solMessage.getSubject(), solMessage.getVerb());
 
+        // 1. Find the appropriate context for the given message subject and verb
+
+        // 2. If one is found, then run its builders
+
+        // 3. With context, search for Message Templates for this Notification Context, filtering by any criteria
+
+        // 4. For each qualifying MessageTemplate, create a Notification/Message delivery
+
+        // 5. For each Message Template, get the content, logging an error those that have merge-fields not in context
+
+        // 6. Begin delivery process for Templates which do not error
 /*
         MessageTemplate crit = new MessageTemplate();
         crit.setEventSubject(solMessage.getSubject());
