3bffaac1f18080b6b90f5c1b996a86e38db4093d
Niqui Eastman <president@solutionsmodular.com>
2023-12-17 22:19:06 -0700

    Init admin project

---
+++ admin/src/main/java/com/solmod/notifications/admin/NotificationEngineAdminServicesApplication.java
@@ -0,0 +1,13 @@
+package com.solmod.notifications.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationEngineAdminServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationEngineAdminServicesApplication.class, args);
+	}
+
+}
---
+++ admin/src/test/java/com/solmod/notifications/admin/NotificationEngineAdminServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solmod.notifications.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationEngineAdminServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
